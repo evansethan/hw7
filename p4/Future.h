d1f6d04981f72fe7aee7d2f0af819989e16b4f37
Ethan Evans <ethanevans.j@gmail.com>
2026-02-19 23:58:49 -0600

    p4 start

--- p4/Future.h
+++ p4/Future.h
@@ -1,1 +1,18 @@
-//todo
+#ifndef FUTURE_H
+#define FUTURE_H
+
+#include <memory>
+#include <exception>
+
+
+template<typename T, typename Func>
+class my_future {
+public:
+    std::shared_ptr sptr;
+    std::exception_ptr eptr;
+
+    my_future() : x(x) {};
+
+};
+
+#endif
