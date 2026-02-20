b96e63e30ad98abcb99dc58af0769dfabf2762a2
Ethan Evans <ethanevans.j@gmail.com>
2026-02-19 23:37:50 -0600

    p3

--- p3/PackagedTask.h
+++ p3/PackagedTask.h
@@ -5,54 +5,22 @@
 #include <thread>
 #include <vector>
 
+template<typename T, typename Func>
+class my_packaged_task {
+public:
+    std::promise<T> promise;
+    Func f;
+    my_packaged_task(Func func) : f(func) {};
 
-template<typename Func, typename ...Args>
-auto my_packaged_task(Func f, Args... args) {
-
-    std::promise<> promise;
-    std::future<auto> future = promise.get_future();
-
-}
+    std::future<T> get_future() {
+        return promise.get_future();
+    }
+    
+    template<typename ...Args>
+    void operator()(Args... args) {
+        promise.set_value(f(args...));
+    }
+};
 
 
 #endif
-
-
-
- 
-void accumulate(std::vector<int>::iterator first,
-                std::vector<int>::iterator last,
-                std::promise<int> accumulate_promise)
-{
-    int sum = std::accumulate(first, last, 0);
-    accumulate_promise.set_value(sum); // Notify future
-}
- 
-void do_work(std::promise<void> barrier)
-{
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    barrier.set_value();
-}
- 
-int main()
-{
-    // Demonstrate using promise<int> to transmit a result between threads.
-    std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
-    std::promise<int> accumulate_promise;
-    std::future<int> accumulate_future = accumulate_promise.get_future();
-    std::thread work_thread(accumulate, numbers.begin(), numbers.end(),
-                            std::move(accumulate_promise));
- 
-    // future::get() will wait until the future has a valid result and retrieves it.
-    // Calling wait() before get() is not needed
-    // accumulate_future.wait(); // wait for result
-    std::cout << "result=" << accumulate_future.get() << '\n';
-    work_thread.join(); // wait for thread completion
- 
-    // Demonstrate using promise<void> to signal state between threads.
-    std::promise<void> barrier;
-    std::future<void> barrier_future = barrier.get_future();
-    std::thread new_work_thread(do_work, std::move(barrier));
-    barrier_future.wait();
-    new_work_thread.join();
-}
